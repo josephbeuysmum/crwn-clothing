3adb62d4d3079fa7f06640bd1e695b6e1fefbc6b
Richard Willis <richard@josephbeuysmum.co.uk>
2020-04-18 21:26:16 +0100

    email/psw sign up in firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,27 @@ const config = {
     appId: "1:258467485083:web:d8199c22cacd5a1879fd3e"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapshop = await userRef.get();
+    if (!snapshop.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+        try  {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
