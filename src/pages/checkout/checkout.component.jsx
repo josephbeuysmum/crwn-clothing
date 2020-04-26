36362dc5daac94944087aa7095a28ef3b4664a35
Richard Willis <richard@josephbeuysmum.co.uk>
2020-04-26 14:19:10 +0100

    checkout component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -24,7 +24,7 @@ const CheckoutPage = ({cartItems, total}) => (
                 <span>Remove</span>
             </div>
         </div>
-        {
+        {       
             cartItems.map(cartItem =>
                 <CheckoutItem key={cartItem.id} cartItem={cartItem} />
             )
